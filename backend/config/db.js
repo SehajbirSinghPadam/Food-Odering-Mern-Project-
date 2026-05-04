3650f3fb4e375425a30b11297487e94d0a899f67
SehajbirSingh <155966103+SehajbirSinghPadam@users.noreply.github.com>
2026-05-04 15:07:09 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-export const connectDb =  async ()=>{
-    await mongoose.connect('mongodb+srv://singhsehajbir1906:Sehajbir911@cluster0.xy1mp.mongodb.net/food-del').then(()=>console.log("!!!    Db Connected Suceessfully    |||"));
 
+export const connectDb = async ()=>{
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("!!!! Db Connected Suceessfully    !!!"));
 }
